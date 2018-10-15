6f40ddc4fc25136b24f71025bc4d27aef818f6a2
JaneWB <brownjannick@att.net>
2018-10-15 10:03:43 -0500

    Number Cruncher 13-1 whole solution

--- NumberCruncher13_1/Program.cs
+++ NumberCruncher13_1/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using NumberUtility;
 
-namespace NumberCruncher13_1
+namespace CG__1_13
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> nums = new List<int>() { 10, 5, 8, 6, 7, 2, 14, 3, 4, 22 };
+            //nums.Add(10);
+            //nums.Add(5);
+            //nums.Add(6);
+            //nums.Add(7);
+            //nums.Add(8);
+            //nums.Add(2);
+            //nums.Add(14);
+            //nums.Add(3);
+            //nums.Add(4);
+            //nums.Add(22);
+
+            Console.WriteLine("The sum of all the even numbers is:  \n" + NumberCruncher.SumOfEvens(nums));
+
+            Console.ReadLine();
         }
     }
 }
+
+
