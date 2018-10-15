a7144c1e03a80856c873f28201ed946a3e97ca86
JaneWB <brownjannick@att.net>
2018-10-15 09:58:50 -0500

    Add project files.

---
+++ NumberCruncher13_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumberCruncher13_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
